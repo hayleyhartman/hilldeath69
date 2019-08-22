59d3fbe642af22f05df5bc77a2cd9baa473f20a3
hayleyhartman <hayley.susan.hartman@gmail.com>
2019-08-22 13:31:08 -0400

    gettin crazy

--- javascript.js
+++ javascript.js
@@ -1,8 +1,25 @@
 const ghost = document.getElementById("ghost")
 const words = document.getElementById("words")
+const schedule = document.getElementById("schedule")
+const drops = document.getElementsByClassName("drop")
+const dropsId = document.getElementById("drops")
 
 ghost.addEventListener("mouseover", wordsAppear)
+ghost.addEventListener("mouseout", wordsFade)
+schedule.addEventListener("wheel", makeItRain)
 
 function wordsAppear () {
-    words.style.opacity = "0.8"
+    words.style.opacity = "0.6"
+}
+
+function wordsFade () {
+    setTimeout(function(){
+        words.style.opacity = "0"},
+     10000);
+}
+
+function makeItRain () {
+    for (let i = 0; i < drops.length; i++) {
+        drops[i].classList.add("rain")
+    }
 }
